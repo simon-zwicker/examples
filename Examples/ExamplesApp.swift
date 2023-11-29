ba5d9928e61852b791abbfff47643244ebe4a822
simon-zwicker <github@zwicker.io>
2023-11-29 09:17:05 +0100

    Initial Commit

---
+++ Examples/ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamplesApp.swift
+//  Examples
+//
+//  Created by Simon Zwicker on 29.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
